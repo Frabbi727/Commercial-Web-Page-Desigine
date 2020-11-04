fd889b3110d081f661bfb361533b0a6eacee599c
Frabbi727 <Frabbi727@gmail.com>
2020-11-04 20:12:22 +0600

    Progressing Work on Brand log

---
+++ Script.js
@@ -0,0 +1,12 @@
+// var imageAll=['images/image1.png','images/image.2.jpg','images/image2.jpg','images/image3.jfif','images/image4.jfif'];
+// var index= 0;
+//
+// function demo() {
+//     var mainImage= document.getElementById('priviewImage');
+//     mainImage.setAttribute('src',imageAll[index]);
+//     index++;
+//     if(index>=imageAll.length){
+//         index=0;
+//     };
+// };
+// setInterval(demo,2000);
